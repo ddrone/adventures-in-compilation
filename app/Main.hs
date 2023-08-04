a6d6ce4f20ddd6897ee65f4ae4f40a707f87b076
Andrew Shulaev <ash.drone@gmail.com>
2023-08-04 22:40:38 +0100

    Write down some datatypes

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Data.Int (Int64)
+import Data.Text (Text)
+import qualified Data.Text as Text
+
+type LangInt = Int64
+type Ident = Text
+
+data Exp
+  = Var Ident
+  | Lit LangInt
+  | Call Ident [Exp]
+
+data Stmt
+  = Assign Ident Exp
+  | Return Exp
+
+data Function = Function
+  { fnName :: Ident
+  , fnArgs :: [Ident]
+  , fnBody :: Block
+  }
+
+type Block = [Stmt]
+
 main :: IO ()
 main = putStrLn "Here be dragons"
