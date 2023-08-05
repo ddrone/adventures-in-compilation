fd45a535ff4a9702552b645d79e0cd6395ab43e6
Andrew Shulaev <ash.drone@gmail.com>
2023-08-06 00:21:21 +0100

    Refactor the code

---
+++ app/AST.hs
@@ -0,0 +1,24 @@
+module AST where
+
+import Data.Int (Int64)
+import Data.Text (Text)
+
+type LangInt = Int64
+type Ident = Text
+
+data Exp
+  = Var Ident
+  | Lit LangInt
+  | Call Ident [Exp]
+
+data Stmt
+  = Assign Ident Exp
+  | Return Exp
+
+data Function = Function
+  { fnName :: Ident
+  , fnArgs :: [Ident]
+  , fnBody :: Block
+  }
+
+type Block = [Stmt]
