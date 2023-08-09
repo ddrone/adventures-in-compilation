ae38797a9565f10d5976fc45379f6f1993ffdf17
Andrew Shulaev <ash.drone@gmail.com>
2023-08-09 11:12:39 +0100

    Convert parsed graph to internal data structure

--- app/Graph/Parser.hs
+++ app/Graph/Parser.hs
@@ -1,9 +1,17 @@
 module Graph.Parser where
 
+import Data.IntMap (IntMap)
+import Data.Set (Set)
+import Data.Map (Map)
+import Data.Maybe (fromJust)
 import Data.Text (Text)
+import Data.Tuple (swap)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Data.Text as Text
+import qualified Data.Set as Set
+import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
 
 import Parser (Parser)
 import Graph.Defs (Graph, fromEdges)
@@ -28,3 +36,18 @@ nodeDef = do
   to <- many nodeName
   Parser.symbol ";"
   pure (NodeDef from to)
+
+data ParsedGraph = ParsedGraph
+  { pgNames :: IntMap Text
+  , pgGraph :: Graph
+  }
+
+buildGraph :: [NodeDef] -> ParsedGraph
+buildGraph nodeDefs = do
+  let addNode (NodeDef from to) nodeSet = foldr Set.insert nodeSet (from : to)
+  let allNodes = zip (enumFrom (0 :: Int)) . Set.toList $ foldr addNode Set.empty nodeDefs
+  let nameToId = Map.fromList (swap <$> allNodes)
+  let getId v = fromJust (Map.lookup v nameToId)
+  let nodeToEdges (NodeDef from to) = map ((,) (getId from) . getId) to
+  let graph = fromEdges (concatMap nodeToEdges nodeDefs)
+  ParsedGraph (IntMap.fromList allNodes) graph
