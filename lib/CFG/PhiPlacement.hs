8d2b47a8f2758eb4a51404528b6425632a48d0fd
Andrew Shulaev <ash.drone@gmail.com>
2023-08-15 16:44:51 +0100

    Write down the type of the function, write down an intermediate task

--- lib/CFG/PhiPlacement.hs
+++ lib/CFG/PhiPlacement.hs
@@ -1,9 +1,11 @@
 module CFG.PhiPlacement where
 
+import Control.Monad.ST (runST)
 import Data.Foldable (toList)
 import Data.Text (Text)
 import Data.Set (Set)
 import Data.Map (Map)
+import Data.STRef
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
@@ -30,3 +32,11 @@ addFunction fn used = foldr addBlock used (fnBody fn)
 
 usedVariables :: [Function] -> VariableMap
 usedVariables fns = foldr addFunction Map.empty fns
+
+-- For a block with a particular name, which identifiers should have
+-- phi-nodes inserted when generating the SSA form.
+type PhiMap = Map BlockName [Ident]
+
+phiPlacement :: Function -> PhiMap
+phiPlacement = runST $ do
+  undefined
