b280c47ef9376cb246ac129f8a963f462867ec6b
Andrew Shulaev <ash.drone@gmail.com>
2023-09-06 22:43:39 +0100

    Write down datatype for X86 programs

---
+++ lib/LVar/X86.hs
@@ -0,0 +1,46 @@
+module LVar.X86 where
+import Data.Int (Int64)
+import Data.Text (Text)
+
+data Reg
+  = Rsp
+  | Rbp
+  | Rax
+  | Rbx
+  | Rcx
+  | Rdx
+  | Rsi
+  | Rdi
+  | R8
+  | R9
+  | R10
+  | R11
+  | R12
+  | R13
+  | R14
+  | R15
+  deriving (Eq, Show)
+
+data Arg n
+  = Immediate Int64
+  | Reg Reg
+  | Deref Reg Int64
+  | Name n
+  deriving (Show)
+
+data Instr n
+  = Addq (Arg n) (Arg n)
+  | Subq (Arg n) (Arg n)
+  | Movq (Arg n) (Arg n)
+  | Negq (Arg n)
+  | Pushq (Arg n)
+  | Popq (Arg n)
+  | Callq Text Int64
+  | Retq
+  | Jump Text
+  deriving (Show)
+
+data Program n = Program
+  { progInstrs :: [Instr n]
+  }
+  deriving (Show)
