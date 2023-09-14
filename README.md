4b9fc02a1e95404ffa77115555ce75d4db918912
Andrew Shulaev <ash.drone@gmail.com>
2023-09-14 18:59:42 +0100

    Implement NFA construction

--- README.md
+++ README.md
@@ -20,8 +20,8 @@ Instead of trying to "implement a cool programming language", a project that I h
   - [x] Actually test it on a couple of examples
 - Regular expressions
   - [x] Write down datatypes for regular expressions (WITHOUT RANGES)
-  - [ ] Write down datatypes for NFA
-  - [ ] Implement NFA conversion
+  - [x] Write down datatypes for NFA
+  - [x] Implement NFA conversion
   - [ ] Write down datatypes for DFA
   - [ ] Implement NFA-to-DFA conversion
   - [ ] Implement automata minimization
--- lib/Regular/Expr.hs
+++ lib/Regular/Expr.hs
@@ -1,5 +1,7 @@
 module Regular.Expr where
 
+import Control.Monad.ST
+import Data.STRef
 import Data.Map (Map)
 import Data.IntMap (IntMap)
 import qualified Data.Map as Map
@@ -33,10 +35,54 @@ range s = case s of
 data EdgeLabel
   = EpsLabel
   | CharLabel Char
-  deriving (Show)
+  deriving (Show, Eq, Ord)
 
 data NFA = NFA
   { nfaCount :: Int
   , nfaEdges :: IntMap (Map EdgeLabel Int)
   }
   deriving (Show)
+
+buildNFA :: Re -> NFA
+buildNFA re = runST $ do
+  next <- newSTRef 0
+  let fresh = do
+        result <- readSTRef next
+        writeSTRef next (result + 1)
+        pure result
+  edges <- newSTRef (IntMap.empty :: IntMap (Map EdgeLabel Int))
+  let addEdge from label to = do
+        let add = Map.singleton label to
+        modifySTRef edges (IntMap.insertWith Map.union from (Map.singleton label to))
+  let addEps from to = addEdge from EpsLabel to
+  let go expr = do
+        start <- fresh
+        end <- fresh
+        case expr of
+          Char c -> do
+            addEdge start (CharLabel c) end
+          Epsilon -> do
+            addEps start end
+          Empty -> pure () -- nothing to insert
+          Union r1 r2 -> do
+            (s1, e1) <- go r1
+            (s2, e2) <- go r2
+            addEps start s1
+            addEps start s2
+            addEps e1 end
+            addEps e2 end
+          Concat r1 r2 -> do
+            (s1, e1) <- go r1
+            (s2, e2) <- go r2
+            addEps start s1
+            addEps e1 s2
+            addEps s2 end
+          Iterate r -> do
+            (s, e) <- go r
+            addEps start s
+            addEps e start
+            addEps e end
+            addEps start end
+        pure (start, end)
+  _ <- go re
+  NFA <$> readSTRef next <*> readSTRef edges
