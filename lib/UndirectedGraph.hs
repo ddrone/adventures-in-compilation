40aa7096a2fb9210ac6a85354ac94e1bb56788c8
Andrew Shulaev <ash.drone@gmail.com>
2023-09-17 12:07:29 +0100

    Add module to store undirected graphs

---
+++ lib/UndirectedGraph.hs
@@ -0,0 +1,17 @@
+module UndirectedGraph where
+
+import Data.Set (Set)
+import SetMultimap (SetMultimap)
+import qualified SetMultimap
+
+newtype Graph v = Graph
+  { graphEdges :: SetMultimap v v
+  }
+  deriving (Show)
+
+addEdge :: Ord v => v -> v -> Graph v -> Graph v
+addEdge from to (Graph g) =
+  Graph (SetMultimap.insert from to (SetMultimap.insert to from g))
+
+edgesFrom :: Ord v => v -> Graph v -> Set v
+edgesFrom v (Graph g) = SetMultimap.lookup v g
