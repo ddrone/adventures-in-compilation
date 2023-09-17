0c1e8843d84b7d4f58010293770ff97dd1c842e1
Andrew Shulaev <ash.drone@gmail.com>
2023-09-17 12:41:38 +0100

    Functions to convert UndirectedGraph to Graphviz format

--- lib/UndirectedGraph.hs
+++ lib/UndirectedGraph.hs
@@ -2,11 +2,13 @@ module UndirectedGraph where
 
 import Data.Set (Set)
 import Data.Map (Map)
+import Data.Text (Text)
 import SetMultimap (SetMultimap)
 import qualified SetMultimap
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Control.Monad (guard)
+import qualified Graphviz
 
 newtype Graph v = Graph
   { graphEdges :: SetMultimap v v
@@ -29,3 +31,13 @@ allEdges (Graph g) = do
   to <- Set.toList tos
   guard (from <= to)
   pure (from, to)
+
+printGraph :: Ord v => (v -> v -> Graphviz.Edge) -> Graph v -> Text
+printGraph genEdge g =
+  let edges = map (uncurry genEdge) (allEdges g) in
+  Graphviz.printUndirectedGraph [] edges
+
+printGraphSimple :: Ord v => (v -> Text) -> Graph v -> Text
+printGraphSimple printNode g =
+  let genEdge v u = Graphviz.edge (printNode v) (printNode u) in
+  printGraph genEdge g
