f5136d96224fca2bd00f221ecb49a5339d3c218a
Andrew Shulaev <ash.drone@gmail.com>
2023-09-22 15:47:07 +0100

    Write down a plan for implementing the optimization I have in mind

--- lib/LVar/OptimizeBlocks.hs
+++ lib/LVar/OptimizeBlocks.hs
@@ -23,3 +23,15 @@ countBlockUses (Module start blocks) = do
   let startVerts = tailOuts (blockTail start)
   let startMap = IntMap.fromList (map (flip (,) 1) startVerts)
   go startMap IntSet.empty startVerts
+
+-- A block is inlinable if it either has only tail or is used exactly once.
+
+-- The idea of what I want to implement is as follows:
+-- 1. While optimizing a block, go through the sequences of instructions, using a counter.
+-- 1.1. If a variable gets assigned to a result of comparison, save this in a map (with current counter)
+-- 1.2. If a variable gets assigned to anything, save the last assignment time
+-- 1.3. When having finished with a block:
+-- 1.3.1. If tail is a goto, follow it immediately if it's inlinable.
+-- 1.3.2. If tail is a conditional jump, see if it uses atom as a condition
+--          If condition is both a comparison, and none of the values of comparison have
+--          been overwritten in the mean time, inline the comparison into a goto.
