7751fd1dd8645f7b3324bfbe3dbb92aed18ead43
Andrew Shulaev <ash.drone@gmail.com>
2023-11-04 10:55:58 +0000

    Some progress on testing the parser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   const [msg, setMsg] = useState('');
+  const [lastText, setLastText] = useState('');
+  const textarea = useRef<HTMLTextAreaElement|null>(null);
 
   async function handleClick() {
+    const text = textarea.current!.value;
+    setLastText(text);
     const response = await fetch('/api/parse', {
       method: 'POST',
-      body: 'Program to parse'
+      body: text
     });
     const json = await response.json();
     setMsg(JSON.stringify(json));
@@ -14,10 +18,21 @@ function App() {
 
   return (
     <>
+      <textarea ref={textarea} rows={10} cols={80}>
+      </textarea><br />
       <button onClick={handleClick}>
         Click me
       </button><br />
-      {msg}
+      <div className="output">
+        <div className="text">
+          <pre>
+            {lastText}
+          </pre>
+        </div>
+        <div className="result">
+          {msg}
+        </div>
+      </div>
     </>
   )
 }
